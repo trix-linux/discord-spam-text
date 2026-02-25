476a2b1194b62015ba1f4ca2f91c40af3414b4d2
trix-dev <mint.monka@gmail.com>
2026-02-25 11:38:17 +0100

    Update webhook URL and message content

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 setInterval(() => {
-  fetch('https://discord.com/api/webhooks/1475077480535822336/Q17DfnwoGVbqXPpu5QWGAZbbsrSNiEBQ2kjW60Gz2PLHlOzDZ4_Ii497kILFIJtlfyt5', {
+  fetch('your webhook token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      content: "silverrr she is in vc"
+      content: "the messege you want to send"
     })
   })
   .then(res => console.log("Status:", res.status))
   .catch(console.error);
 
-}, 1500);
+}, 1500); 
